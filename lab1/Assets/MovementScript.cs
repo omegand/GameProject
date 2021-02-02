3519078161eb2c474dd346687783aa2982cc814c
Omegand <piktasdrakonas49@gmail.com>
2021-02-02 14:15:23 +0200

    Added Jump

--- lab1/Assets/MovementScript.cs
+++ lab1/Assets/MovementScript.cs
@@ -6,19 +6,29 @@ public class MovementScript : MonoBehaviour
 {
     CharacterController cont;
     public float speed = 5f;
+    public float gravity;
+    private float VVelocity;
+    private float JForce = 5f;
     float horizontal;
     float vertical;
     void Start()
     {
-        cont = gameObject.GetComponent<CharacterController>();
-        //cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Transform>();
+        cont = GetComponent<CharacterController>();
     }
 
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
-        Vector3 movement = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (cont.isGrounded)
+        {
+            VVelocity = -gravity * Time.deltaTime;
+            if (Input.GetKeyDown(KeyCode.Space))
+                VVelocity = JForce;
+        }
+        else VVelocity -= gravity * Time.deltaTime;
+        Vector3 movement = new Vector3(horizontal, VVelocity, vertical);
         float angle = Mathf.Atan2(movement.x, movement.z) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, angle, 0f);
         cont.Move(movement * speed * Time.deltaTime);
