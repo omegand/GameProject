b4b7c5f257c5301419b674f7a03dbec58ae5ffb1
Omegand <piktasdrakonas49@gmail.com>
2021-02-02 14:03:06 +0200

    Movement and Camera

---
+++ lab1/Assets/MovementScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    CharacterController cont;
+    public float speed = 5f;
+    float horizontal;
+    float vertical;
+    void Start()
+    {
+        cont = gameObject.GetComponent<CharacterController>();
+        //cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+        Vector3 movement = new Vector3(horizontal, 0f, vertical).normalized;
+        float angle = Mathf.Atan2(movement.x, movement.z) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, angle, 0f);
+        cont.Move(movement * speed * Time.deltaTime);
+
+    }
+}
--- lab1/Assets/script/PlayerControler.cs
+++ lab1/Assets/script/PlayerControler.cs
@@ -4,15 +4,4 @@ using UnityEngine;
 
 public class PlayerControler : MonoBehaviour
 {
-    public float moveSpeed = 0.2f;
-    public float rotationSpeed = 2.5f;
-    void FixedUpdate()
-    {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(vertical, 0.0f, 0);
-
-        transform.Translate(movement * moveSpeed);
-        transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y + horizontal * rotationSpeed, 0);
-    }
 }
