0e13d06f08ff793af0149a7b4d5143a3dae92238
Omegand <piktasdrakonas49@gmail.com>
2021-02-02 15:38:19 +0200

    Move to Camera direction

--- lab1/Assets/MovementScript.cs
+++ lab1/Assets/MovementScript.cs
@@ -11,6 +11,8 @@ public class MovementScript : MonoBehaviour
     private float JForce = 5f;
     float horizontal;
     float vertical;
+    float SmoothVelocity;
+    public Transform Cam;
     void Start()
     {
         cont = GetComponent<CharacterController>();
@@ -29,9 +31,8 @@ public class MovementScript : MonoBehaviour
         }
         else VVelocity -= gravity * Time.deltaTime;
         Vector3 movement = new Vector3(horizontal, VVelocity, vertical);
-        float angle = Mathf.Atan2(movement.x, movement.z) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, angle, 0f);
-        cont.Move(movement * speed * Time.deltaTime);
+        Vector3 adjustedmovement = Cam.TransformDirection(movement);
+        cont.Move(adjustedmovement * speed * Time.deltaTime);
 
     }
 }
