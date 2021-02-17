b4ccc218f1cef0bb92d6c8f1da81ebbff6636dc5
Omegand <piktasdrakonas49@gmail.com>
2021-02-18 00:14:53 +0200

    Added double jump and camera lock

---
+++ GameProj/Assets/LockCam.cs
@@ -0,0 +1,31 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockCam : MonoBehaviour
+{
+    public CinemachineFreeLook cameraLook;
+    private void Start()
+    {
+        cameraLook = gameObject.GetComponent<CinemachineFreeLook>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            cameraLook.m_YAxis.m_InputAxisName = "Mouse Y";
+            cameraLook.m_XAxis.m_InputAxisName = "Mouse X";
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            cameraLook.m_YAxis.m_InputAxisName = "";
+            cameraLook.m_XAxis.m_InputAxisName = "";
+            cameraLook.m_YAxis.m_InputAxisValue = 0;
+            cameraLook.m_XAxis.m_InputAxisValue = 0;
+        }
+    }
+
+    
+}
--- GameProj/Assets/Scripts/Movement.cs
+++ GameProj/Assets/Scripts/Movement.cs
@@ -4,31 +4,34 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public Transform Ground;
+
     public LayerMask WhatIsGround;
     public float speed = 8f;
-    public Transform Cam;
     public float JumpForce = 4f;
-    float gravity = -25f;
+    public float gravity = -35f;
     CharacterController cont;
     Vector3 VVel;
+    Transform Ground;
+    Transform Cam;
     bool Grounded;
-    private float GCheckRadius = 0.4f;
-
-
-    private Animator animator;
+    float GCheckRadius = 0.4f;
+    int Jumps = 0;
+    Animator animator;
 
 
     void Start()
     {
         cont = GetComponent<CharacterController>();
+        Cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Transform>();
+        Ground = GameObject.Find("GroundCheck").GetComponent<Transform>();
         animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         Grounded = Physics.CheckSphere(Ground.position, GCheckRadius, WhatIsGround);
-        if (Grounded && VVel.y < 0) VVel.y = -1f;
+        if (Grounded && VVel.y < 0) { VVel.y = -1f; Jumps = 2; }
+
         Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         if (movement.magnitude > 0)
         {
@@ -42,7 +45,12 @@ public class Movement : MonoBehaviour
         {
             animator.SetBool("walking", false);
         }
-        if (Input.GetKeyDown(KeyCode.Space) && Grounded) VVel.y = Mathf.Sqrt(JumpForce * -2f * gravity);
+        if (Input.GetKeyDown(KeyCode.Space) && Jumps > 0) 
+        {
+            VVel.y = Mathf.Sqrt(JumpForce * -2f * gravity);
+            Jumps--;
+        }
+
         VVel.y += gravity * Time.deltaTime;
         cont.Move(VVel * Time.deltaTime);
 
