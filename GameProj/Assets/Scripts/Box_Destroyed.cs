eb33db1a72b0ce0929df575d58dd2fd52955b15a
Omegand <piktasdrakonas49@gmail.com>
2021-02-23 02:13:40 +0200

    Fixed combat scene, refactoring even more, cleaned up garbage

---
+++ GameProj/Assets/Scripts/Box_Destroyed.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box_Destroyed : MonoBehaviour
+{
+    GameObject HealthPickup;
+    ParticleSystem boxExplode;
+    Vector3 boxCenter;
+    void Start()
+    {
+        HealthPickup = Resources.Load<GameObject>("Prefabs/Bottle_Health");
+        boxExplode = Resources.Load<ParticleSystem>("Particles/BoxDestroy");
+        boxCenter = GetComponent<BoxCollider>().center;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Vector3 newPosition = new Vector3();
+            newPosition.x = Mathf.Clamp(transform.position.x, transform.position.x, transform.position.x + boxCenter.x);
+            newPosition.y = transform.position.y;
+            newPosition.z = Mathf.Clamp(transform.position.z, transform.position.z, transform.position.z + boxCenter.z);
+            Instantiate(HealthPickup, newPosition, transform.rotation);
+            boxExplode.transform.position = newPosition;
+            boxExplode.GetComponent<ParticleSystem>().Play();
+            Destroy(gameObject);
+        }
+    }
+}
