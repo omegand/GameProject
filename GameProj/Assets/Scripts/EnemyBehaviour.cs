fe4615500b0a28518199fa9e307c809b95b4a01a
Omegand <piktasdrakonas49@gmail.com>
2021-03-15 13:08:29 +0200

    animations and color changing

--- GameProj/Assets/Scripts/EnemyBehaviour.cs
+++ GameProj/Assets/Scripts/EnemyBehaviour.cs
@@ -7,12 +7,16 @@ public class EnemyBehaviour : MonoBehaviour
 {
     private NavMeshAgent enemy;
     private GameObject player;
-    [SerializeField] private float sightRange,patrolVariance;
+    [SerializeField] private float sightRange, patrolVariance;
     private bool seen = false;
     private Vector3 patrolPos, startPos;
     private bool patrolling = false;
+    private Animator anim;
+    private Light light;
     private void Start()
     {
+        light = transform.Find("Spot Light").GetComponent<Light>();
+        anim = GetComponent<Animator>();
         startPos = transform.position;
         enemy = GetComponent<NavMeshAgent>();
         player = GameObject.FindGameObjectWithTag("Player");
@@ -20,8 +24,18 @@ public class EnemyBehaviour : MonoBehaviour
     private void Update()
     {
         seen = Physics.CheckSphere(transform.position, sightRange, LayerMask.GetMask("Player"));
-        if (!seen && !patrolling) { InvokeRepeating("Patrolling", 0, 4f); patrolling = true; }
-        if(seen) Chasing();
+        if (!seen && !patrolling)
+        {
+            InvokeRepeating("Patrolling", 0, 4f);
+            patrolling = true;
+            anim.SetBool("moving", false);
+            light.color = Color.cyan;
+        }
+        if (seen) {
+            Chasing();
+            anim.SetBool("moving", true);
+            light.color = Color.red;
+        }
     }
     void Patrolling()
     {
@@ -34,7 +48,7 @@ public class EnemyBehaviour : MonoBehaviour
         {
             patrolPos = new Vector3(
                 transform.position.x + Random.Range(-patrolVariance, patrolVariance),
-                transform.position.y, 
+                transform.position.y,
                 transform.position.z + Random.Range(-patrolVariance, patrolVariance)
                 );
         }
