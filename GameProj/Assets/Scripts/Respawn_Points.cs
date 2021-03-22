ad51cc0d190aeec5a265ea9099df872d5e6344c0
KarkiusX <kulkasta7@gmail.com>
2021-03-22 23:29:09 +0200

    Added trap

--- GameProj/Assets/Scripts/Respawn_Points.cs
+++ GameProj/Assets/Scripts/Respawn_Points.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms;
 
 public class Respawn_Points : MonoBehaviour
 {
@@ -16,19 +18,56 @@ public class Respawn_Points : MonoBehaviour
     [SerializeField]
     [Range(0, 1)]
     private double SpawnChance;
+    private int lastPropIndex;
 
     private GameObject[] foundProps;
     private void Awake()
     {
         foundProps = GameObject.FindGameObjectsWithTag(SpawnPointTag);
+        lastPropIndex = -1;
     }
     private void Start()
     {
+        int propCount = props.Count;
+        int failedCount = 0;
+        GameObject lastSpawn = null;
         foreach(GameObject gameObject in foundProps)
         {
-            GameObject item = Instantiate(props[0]);
+            int ShouldSpawn = UnityEngine.Random.Range(0, 1);
+            if (ShouldSpawn > SpawnChance)
+                continue;
+
+            int objectIndex = UnityEngine.Random.Range(0, propCount - 1);
+            if(lastPropIndex != -1)
+            {
+                if(!SamePropWithinDistance)
+                {
+                    while(objectIndex == lastPropIndex)
+                    {
+                        objectIndex = UnityEngine.Random.Range(0, propCount - 1);
+                        failedCount += 1;
+
+                        if (failedCount >= 3)
+                            break;
+                    }
+                }
+            }
+            if(failedCount >= 3)
+            {
+                failedCount = 0;
+                continue;
+            }
+            if(lastSpawn != null)
+            {
+                if (Vector3.Distance(gameObject.transform.position, lastSpawn.transform.position) <= DistanceBetweenProp)
+                    continue;
+            }
+            GameObject item = Instantiate(props[objectIndex]);
+            lastPropIndex = objectIndex;
+            lastSpawn = item;
             item.transform.position = gameObject.transform.position;
             item.transform.rotation = gameObject.transform.rotation;
+            failedCount = 0;
         }
     }
     // Update is called once per frame
---
+++ GameProj/Assets/Trap_Activate.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Trap_Activate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    [Range(0, 1)]
+    private float ChanceToActivate;
+    [SerializeField]
+    private GameObject enemy;
+    private float onGround;
+    private List<GameObject> rewards;
+
+    [SerializeField]
+    private float Force;
+    private CharacterController character;
+
+    private Vector3 impact = Vector3.zero;
+
+    private bool Active;
+
+
+    private void Awake()
+    {
+        Active = true;
+        rewards = new List<GameObject>();
+    }
+    private void Start()
+    {
+        character = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterController>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Active)
+            return;
+        if (impact.magnitude > 0.2)
+        {
+            character.Move(impact * Time.deltaTime);
+        }
+        impact = Vector3.Lerp(impact, Vector3.zero, 5 * Time.deltaTime);
+    }
+    private void AddImpact(Vector3 force)
+    {
+        Debug.Log(force);
+        impact += force * force.magnitude;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!Active)
+            return;
+        GameObject gobject = other.gameObject;
+        if(gobject.tag.Equals("Reward"))
+        {
+            if (!rewards.Contains(gobject.transform.parent.gameObject))
+            {
+                rewards.Add(gobject.transform.parent.gameObject);
+            }
+        }
+        if(gobject.tag.Equals("Player"))
+        {
+            if(PlayerPrefs.GetInt("InTrap") == 1)
+            {
+                PlayerPrefs.DeleteAll();
+                gameObject.transform.GetChild(0).transform.gameObject.SetActive(false);
+                Active = false;
+                return;
+            }
+            double GetChance = UnityEngine.Random.Range(0, 1);
+            if (GetChance > ChanceToActivate)
+                return;
+
+            AddImpact(character.velocity.normalized * Force);
+
+            gameObject.transform.GetChild(0).transform.gameObject.SetActive(true);
+            Suprise();
+        }
+        if(gobject.tag.Equals("Enemy"))
+        {
+            gobject.GetComponent<BattleSwitcher>().enabled = true;
+            gobject.GetComponent<NavMeshAgent>().enabled = true;
+            gobject.GetComponent<EnemyBehaviour>().enabled = true;
+        }
+    }
+    private void Suprise()
+    {
+        PlayerPrefs.SetInt("InTrap", 1);
+        if(enemy != null)
+        {
+            Instantiate(enemy);
+            enemy.transform.position = gameObject.transform.GetChild(1).transform.position;
+        }
+        else
+        {
+            gameObject.transform.GetChild(0).transform.gameObject.SetActive(false);
+            gameObject.GetComponent<BoxCollider>().isTrigger = false;
+        }
+    }
+}
