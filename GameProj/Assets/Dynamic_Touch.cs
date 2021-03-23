cd3f54f9f3f5ffb9a691eb3d575a741f1de37822
KarkiusX <kulkasta7@gmail.com>
2021-03-23 11:02:38 +0200

    Touch

---
+++ GameProj/Assets/Dynamic_Touch.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dynamic_Touch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag.Equals("Dynamic_Enemy"))
+        {
+            Debug.Log("Huh");
+        }
+    }
+}
