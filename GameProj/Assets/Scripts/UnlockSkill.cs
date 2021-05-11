1f419b5cf8fd2945ef94266e13546ea34763bb27
Omegand <sarunyss@gmail.com>
2021-05-11 09:15:04 +0300

    added skill locking

---
+++ GameProj/Assets/Scripts/UnlockSkill.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnlockSkill : MonoBehaviour
+{
+    Stats player;
+    public int requiredlvl;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Stats>();
+        if(player.level >= requiredlvl)
+        GetComponent<Button>().interactable = true;
+        else GetComponent<Button>().interactable = false;
+    }
+}
