fbddaf0b36588451d79050fe9548a80c1d6dbf8b
KarkiusX <kulkasta7@gmail.com>
2021-05-11 10:15:06 +0300

    Added fullscreen

--- GameProj/Assets/Scripts/Resolution.cs
+++ GameProj/Assets/Scripts/Resolution.cs
@@ -8,10 +8,19 @@ public class Resolution : MonoBehaviour
 {
     [SerializeField]
     private TMPro.TMP_Dropdown dropdownMenu;
+
+    [SerializeField]
+    private UnityEngine.UI.Toggle toggle;
     UnityEngine.Resolution[] resolutions;
     private int currentRez = 0;
+    private int FullScreen = 0;
     void Start()
     {
+        if (PlayerPrefs.GetInt("Fullscreen") == 1)
+            toggle.isOn = true;
+        else
+            toggle.isOn = false;
+
         resolutions = Screen.resolutions;
         dropdownMenu.ClearOptions();
         List<string> options = new List<string>();
@@ -36,5 +45,18 @@ public class Resolution : MonoBehaviour
     {
         UnityEngine.Resolution rez = resolutions[currentRez];
         Screen.SetResolution(rez.width, rez.height, true);
+    }
+    public void ToggleFullScreen(bool yes)
+    {
+        if(toggle.isOn)
+        {
+            Screen.fullScreen = true;
+            PlayerPrefs.SetInt("Fullscreen", 1);
+        }
+        else
+        {
+            Screen.fullScreen = false;
+            PlayerPrefs.SetInt("Fullscreen", 0);
+        }
     }
 }
