199fbcc1db4e8c79e67108c83883281354097ef2
Omegand <sarunyss@gmail.com>
2021-05-23 22:30:16 +0300

    done with key minigame

--- GameProj/Assets/Scripts/Inventory.cs
+++ GameProj/Assets/Scripts/Inventory.cs
@@ -11,7 +11,6 @@ public class Inventory : MonoBehaviour
         Occupied = new bool[3];
         slots = new GameObject[3];
         Transform canvas = transform.Find("MainScreenText").Find("Canvas");
-        Debug.Log(slots.Length);
         for (int i = 0; i < canvas.childCount; i++)
         {
             slots[i] = canvas.GetChild(i).gameObject;
