b4b0ca7642c1ac05bfe97aeb87dc3a469488b30c
Omegand <sarunyss@gmail.com>
2021-05-23 09:35:25 +0300

    add props

---
+++ GameProj/Assets/WIPTEXT.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WIPTEXT : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        ScrollingText.StartSentence(new string[] { "You reached the end... Work in progress." }, new string[] { "Main" });
+    }
+}
