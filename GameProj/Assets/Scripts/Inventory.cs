c10253c6ecae4620a6060ee18e269cdc07d18ce3
Omegand <sarunyss@gmail.com>
2021-05-23 22:04:37 +0300

    finished inventory system

---
+++ GameProj/Assets/Scripts/Inventory.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public bool[] Occupied;
+    public GameObject[] slots;
+    void Start()
+    {
+        Occupied = new bool[3];
+        slots = new GameObject[3];
+        Transform canvas = transform.Find("MainScreenText").Find("Canvas");
+        Debug.Log(slots.Length);
+        for (int i = 0; i < canvas.childCount; i++)
+        {
+            slots[i] = canvas.GetChild(i).gameObject;
+        }
+    }
+}
